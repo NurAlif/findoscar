40b23277ef610d396c4f4c5346bd6c8ff1ff7240
NurAlif <nuralif.ilyasa@gmail.com>
2016-10-16 15:41:23 +0800

    FIRTS COMMIT STILL R A W

---
+++ www/game.js
@@ -0,0 +1,47 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+;
+var master = {
+    media : document.getElementById("mascavs"),
+    devWidth : document.documentElement.clientWidth,
+    devHeight : document.documentElement.clientHeight,
+    start : function(){
+        
+    },
+    clear : function(){
+        
+    }
+};
+
+var box = function(){
+    this.posx;
+    this.posy;
+    this.cont;
+    this.tarx;
+    this.tary;
+};
+
+//pre defined variables
+
+var speed;
+var sumbox;
+var scale;
+
+//undefined variavles
+
+var code42;
+
+//dinamic variables
+
+var cond;
+var steep;
+
+var runtime = function(){
+    while(cond === true){
+        
+    }
+    
+};
